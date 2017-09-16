7d3b840d6378101fac1c14aed8e61be27e94be5c
Hernani <hernanif1@gmail.com>
2017-09-16 20:27:31 -0300

    sign refactor

--- App/Containers/SignInScreen.js
+++ App/Containers/SignInScreen.js
@@ -1,45 +1,46 @@
 import React, { Component } from 'react'
-import { Image } from 'react-native'
-import SplashScreen from 'react-native-splash-screen'
-import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin'
+import { Image, StatusBar, Keyboard } from 'react-native'
 import { NavigationActions } from 'react-navigation'
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin'
 import { connect } from 'react-redux'
+import SplashScreen from 'react-native-splash-screen'
 
-import { Images } from '../Themes'
+import { googleSigInOptions } from 'doit/App/Config/GoogleSigInConfig'
+import { Images } from 'doit/App/Themes'
 import styles from './Styles/LoginScreenStyles'
 
 class SignIn extends Component {
-  async componentDidMount () {
-    setTimeout(function () {
-      SplashScreen.hide()
-    }, 3000)
-
-    await GoogleSignin.configure({
-      // iosClientId: <FROM DEVELOPER CONSOLE>, // only for iOS
-    })
-
-    let user = GoogleSignin.currentUserAsync()
-    if (user) {
-      this.props.goToList()
-    }
+  static navigationOptions = {
+    header: null
   }
 
-  async _signIn () {
-    let user = await GoogleSignin.signIn()
-    console.log('USER LOGGED', user)
-    console.log(this.props)
-    this.props.goToList()
+  async handleUser () {
+    const user = await GoogleSignin.currentUserAsync()
+    if (user) this.props.goToListScreen()
+  }
+
+  async handleSignIn () {
+    await GoogleSignin.signIn()
+    this.props.goToListScreen()
+  }
+
+  async componentDidMount () {
+    Keyboard.dismiss()
+    await GoogleSignin.configure(googleSigInOptions)
+    await this.handleUser()
+    SplashScreen.hide()
   }
 
   render () {
     return (
       <Image source={Images.background} resizeMode='cover' style={[styles.backgroundImage, styles.centered]}>
+        <StatusBar hidden />
         <Image source={Images.logo} style={styles.logo} />
         <GoogleSigninButton
           style={{ width: 312, height: 56, marginTop: 60 }}
           size={GoogleSigninButton.Size.Wide}
           color={GoogleSigninButton.Color.Light}
-          onPress={this._signIn.bind(this)} />
+          onPress={this.handleSignIn.bind(this)} />
       </Image>
     )
   }
@@ -51,7 +52,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    goToList: () => dispatch(NavigationActions.navigate({ routeName: 'ListScreen' }))
+    goToListScreen: () => dispatch(NavigationActions.navigate({ routeName: 'ListScreen' }))
   }
 }
 
