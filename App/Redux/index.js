511f24ba66cd23abc31cf2f50a8ab1c2a0d3320b
Hernani <hernanif1@gmail.com>
2017-09-17 07:42:36 -0300

    update redux reducers

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -6,8 +6,7 @@ export default () => {
   /* ------------- Assemble The Reducers ------------- */
   const rootReducer = combineReducers({
     nav: require('./NavigationRedux').reducer,
-    github: require('./GithubRedux').reducer,
-    search: require('./SearchRedux').reducer
+    google: require('./GoogleRedux').reducer
   })
 
   return configureStore(rootReducer, rootSaga)
