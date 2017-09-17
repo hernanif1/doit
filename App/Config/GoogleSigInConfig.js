b57745498aaa73130e3401214f56b66d024880c0
Hernani <hernanif1@gmail.com>
2017-09-17 07:43:08 -0300

    sigin update

--- App/Config/GoogleSigInConfig.js
+++ App/Config/GoogleSigInConfig.js
@@ -1,5 +1,11 @@
+import Config from 'react-native-config'
 
 export default {
-  scopes: ['https://www.googleapis.com/auth/tasks']
+  scopes: [
+    'https://www.googleapis.com/auth/tasks',
+    'https://www.googleapis.com/auth/tasks.readonly'
+  ],
+  apiKey: Config.API_KEY,
+  clientId: Config.CLIENT_ID
   // iosClientId: <FROM DEVELOPER CONSOLE>, // only for iOS
 }
