58c76841c76ae27640b150706d058839cdeffda5
Hernani <hernanif1@gmail.com>
2017-09-19 21:52:35 -0300

    update google sigin

--- App/Config/GoogleSigInConfig.js
+++ App/Config/GoogleSigInConfig.js
@@ -2,10 +2,9 @@ import Config from 'react-native-config'
 
 export default {
   scopes: [
-    'https://www.googleapis.com/auth/tasks',
-    'https://www.googleapis.com/auth/tasks.readonly'
+    'https://www.googleapis.com/auth/drive.readonly'
   ],
   apiKey: Config.API_KEY,
-  clientId: Config.CLIENT_ID
+  webClientId: '985943679296-hbku6vr7f6perfilcttc2a7outnaro5n.apps.googleusercontent.com'
   // iosClientId: <FROM DEVELOPER CONSOLE>, // only for iOS
 }
